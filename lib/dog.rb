1181ca414dde3dd4fdc0b28d98590f0e5cb98cb6
Nolan Hughes <nhughes987@gmail.com>
2018-01-10 18:02:39 +0000

    automatically backed up by learn

--- lib/dog.rb
+++ lib/dog.rb
@@ -48,7 +48,9 @@ class Dog
   def self.find_by_id(id)
     sql = "SELECT * FROM dogs WHERE ID = ?"
 
-    DB[:conn].execute(sql, id)
+    row = DB[:conn].execute(sql, id)
+
+    self.new_from_db(row)
   end
 
   def self.new_from_db(dog_row)
@@ -59,7 +61,9 @@ class Dog
 
   def self.find_by_name(name)
     sql = "SELECT * FROM dogs WHERE name = ?"
+
     row = DB[:conn].execute(sql, name)[0]
+    
     self.new_from_db(row)
   end
 
