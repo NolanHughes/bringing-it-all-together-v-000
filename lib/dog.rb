09ac64fe92854cfebcf912ef60a9bac987eac45b
Nolan Hughes <nhughes987@gmail.com>
2018-01-10 17:42:06 +0000

    automatically backed up by learn

--- lib/dog.rb
+++ lib/dog.rb
@@ -34,9 +34,8 @@ class Dog
       SQL
       # binding.pry
 
-      dog = DB[:conn].execute(sql, self.name, self.breed)
+      DB[:conn].execute(sql, self.name, self.breed)
       @id = DB[:conn].execute("SELECT last_insert_rowid() FROM dogs")[0][0]
-      dog
     # end
   end
 end
